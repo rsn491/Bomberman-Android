d5579f7a468a1b3c544d823626035a4858abf19c
rsn491 <rsn491@gmail.com>
2014-05-06 17:46:53 +0100

    P2P shit skeleton

---
+++ src/ist/meic/cm/bomberman/p2p/p2pBroadcastReceiver.java
@@ -0,0 +1,87 @@
+package ist.meic.cm.bomberman.p2p;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+import android.util.Log;
+
+public class p2pBroadcastReceiver extends BroadcastReceiver {
+
+	private WifiP2pManager mManager;
+	private Channel mChannel;
+	private InGameP2P activity;
+	private PeerListListener peerListListener;
+	
+	public p2pBroadcastReceiver(WifiP2pManager manager, Channel channel, InGameP2P activity, PeerListListener peerListListener) {
+		super();
+		this.mManager = manager;
+		this.mChannel = channel;
+		this.activity = activity;
+		this.peerListListener = peerListListener;
+		// this.context= context;
+	}
+	 
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Determine if Wifi P2P mode is enabled or not, alert
+            // the Activity.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+//            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+//                activity.setIsWifiP2pEnabled(true);
+//            } else {
+//                activity.setIsWifiP2pEnabled(false);
+//            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            // Request available peers from the wifi p2p manager. This is an
+            // asynchronous call and the calling activity is notified with a
+            // callback on PeerListListener.onPeersAvailable()
+            if (mManager != null) {
+                mManager.requestPeers(mChannel, peerListListener);
+            }
+            Log.d("WifiDirect", "P2P peers changed");
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+        	if (mManager == null) {
+                return;
+            }
+
+            NetworkInfo networkInfo = (NetworkInfo) intent
+                    .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+            if (networkInfo.isConnected()) {
+
+                // We are connected with the other device, request connection
+                // info to find group owner IP
+
+//                mManager.requestConnectionInfo(mChannel, connectionListener);
+            }
+            // Connection state changed!  We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+//            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
+//                    .findFragmentById(R.id.frag_list);
+//            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+//                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
+
+        }
+        
+	}
+
+
+}
